600a8b1c691bcd6b7f582a263251a77f4ca27ba6
Pavlo Vozniuk <antipsih@ukr.net>
2017-08-02 11:46:45 +0000

    fix export

--- src/index.js
+++ src/index.js
@@ -2,5 +2,7 @@
 
 // Babel6 does not hack the default behaviour of ES Modules anymore, so we should export
 const ReactFreeCarousel = require('./Component').default;
+const ReactFreeCarouselTile = require('./Component').ReactFreeCarouselTile;
 
-module.exports = ReactFreeCarousel;
+module.exports = {ReactFreeCarouselTile};
+module.exports.default = ReactFreeCarousel;
